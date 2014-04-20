e4dbadd6f15748d0f4be9581abd4d071f1a0d060
FScoward <FScoward@FSCOWARD-PC>
2014-04-21 00:58:29 +0900

    qa部分をちょこっと作成

--- app/controllers/Question.scala
+++ app/controllers/Question.scala
@@ -4,8 +4,45 @@ package controllers
  * Created by FScoward on 2014/04/19.
  */
 import play.api.mvc.{Controller, Action}
+import play.api.data.Form
+import play.api.data.Forms._
+
 object Question extends Controller {
   def index(projectId: Int) = Action {
-    Ok(views.html.qa())
+    Ok(views.html.qa()).withSession(
+      "projectId" -> projectId.toString
+    )
+  }
+  
+  def makeQuestion = Action { implicit request =>
+    val questionForm = Form(
+      tuple(
+        "from" -> text,
+        "to" -> text,
+        "question" -> text,
+        "limit" -> date("YYYY/MM/DD"),
+        "status" -> text
+      )
+    )
+  
+    
+    questionForm.bindFromRequest.fold(
+      error => {
+        request.session.get("projectId") match {
+          case Some(p) => {
+             Redirect(routes.Question.index(p.toInt)).flashing(
+              "error" -> "Error"
+            )   
+          }
+          case None => {
+            Ok
+          }
+        }
+      },
+      success => {
+        play.Logger.debug("ID: " + request.path)
+        Ok
+      }
+    )
   }
 }
